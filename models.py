e6d7a954638f82bf868041173caf37fd95f347a6
BogdanX1337 <sabaubogdan15@gmail.com>
2025-06-21 21:16:35 +0300

    models.py

--- models.py
+++ models.py
@@ -2,13 +2,17 @@ from datetime import datetime
 
 class Masina:
     def __init__(self, nr_inmatriculare, timestamp=None):
+        # Reprezintă o mașină parcată
+        # Salvează numărul de înmatriculare și timpul parcării (implicit timpul curent)
         self.nr_inmatriculare = nr_inmatriculare
         self.timestamp = timestamp or datetime.now()
 
 class LocParcare:
     def __init__(self, numar, masina=None):
+        # Reprezintă un loc de parcare, cu număr și mașină asociată (dacă există)
         self.numar = numar
         self.masina = masina
 
     def este_liber(self):
+        # Returnează True dacă locul nu are mașină asociată (este liber)
         return self.masina is None
